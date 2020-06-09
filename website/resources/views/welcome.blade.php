cc4acb2659a8d6f284b63c8365b33a8976a1baf5
laullier <hugo.laullier@gmail.com>
2020-06-09 14:35:06 -0400

    cration of the footer

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -8,4 +8,225 @@
 
 @section('content')
 
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner" height="100%">
+            <div class="carousel-item active">
+                <img src="images/actu/actu.jpeg" alt="Actualité 1", style="background-size: cover;">
+                <div class="carousel-caption">
+                    <h1>Info Telecom Strasbourg</h1>
+                    <br>
+                    <p>Notre toute nouvelle association débarque à TPS</p>
+                    <br>
+                    <input class="favorite styled" type="button" value="Découvrir nos projets">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/actu/actu.jpeg" alt="Actualité 2">
+                <div class="carousel-caption">
+                    <h1>Info Telecom Strasbourg</h1>
+                    <br>
+                    <p>Notre toute nouvelle association débarque à TPS</p>
+                    <br>
+                    <input class="favorite styled" type="button" value="Découvrir nos projets">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/actu/actu.jpeg" alt="Actualité 3">
+                <div class="carousel-caption">
+                    <h1>Info Telecom Strasbourg</h1>
+                    <br>
+                    <p>Notre toute nouvelle association débarque à TPS</p>
+                    <br>
+                    <input class="favorite styled" type="button" value="Découvrir nos projets">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="images/actu/actu.jpeg"  alt="Actualité 4">
+                <div class="carousel-caption">
+                    <h1>Info Telecom Strasbourg</h1>
+                    <br>
+                    <p>Notre toute nouvelle association débarque à TPS</p>
+                    <br>
+                    <input class="favorite styled" type="button" value="Découvrir nos projets">
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
 @endsection
