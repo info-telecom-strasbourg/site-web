868ea3784b58d42cc83c1f9dcaa01d0f22d1b8e0
laullier <hugo.laullier@gmail.com>
2020-06-09 03:40:00 -0400

    creation of the navbar

--- website/resources/views/layouts/layout.blade.php
+++ website/resources/views/layouts/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,6 +15,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
     <link rel="stylesheet" href="{{URL::asset('css/fonts.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 
     <!-- JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -22,10 +24,12 @@
 
     <title>@yield('title')</title>
 </head>
+
 <body>
 
     <!-- Navigation bar -->
     @include('navbar')
+
 
     <!-- Main content -->
     <div class="page">
@@ -45,15 +49,4 @@
     @include('footer')
 </body>
 
-<!-- Sticky navbar -->
-<script>
-    window.onscroll = () => {
-        const nav = document.querySelector('.navbar');
-        var sticky = nav.offsetTop;
-        if(this.scrollY > sticky)
-            nav.classList.add('scroll');
-        else
-            nav.classList.remove('scroll');
-    };
-</script>
 </html>
