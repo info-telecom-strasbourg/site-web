5eb3c50cfacdfba82ea9713f5ba5820a397e1beb
laullier <hugo.laullier@gmail.com>
2020-06-09 14:35:06 -0400

    cration of the footer

--- website/resources/views/layouts/layout.blade.php
+++ website/resources/views/layouts/layout.blade.php
@@ -9,15 +9,13 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 
     <!-- Font awesome icons -->
-    <link href="{{ URL::asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('lib/fontawesome/css/all.css') }}" rel="stylesheet">
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
     <link rel="stylesheet" href="{{URL::asset('css/fonts.css')}}">
 
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <link rel="stylesheet" type="text/css" href="css/actu.css">
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 
     <!-- JavaScript -->
@@ -32,229 +30,14 @@
 
     <!-- Navigation bar -->
     @include('navbar')
-
-
-
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-        </ol>
-        <div class="carousel-inner" height="100%">
-            <div class="carousel-item active">
-                <img src="images/actu/actu.jpeg" width="100%" height="100%" alt="Actualité 1", style="background-size: cover;">
-                <div class="carousel-caption d-none d-md-block">
-                    <h1>Info Telecom Strasbourg</h1>
-                    <p>Notre toute nouvelle association débarque à TPS</p>
-                    <input class="favorite styled" type="button" value="Découvrir nos projets">
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/actu/actu.jpeg" width="100%" height="100%" alt="Actualité 2">
-                <div class="carousel-caption d-none d-md-block">
-                    <h1>Info Telecom Strasbourg</h1>
-                    <p>Notre toute nouvelle association débarque à TPS</p>
-                    <input class="favorite styled" type="button" value="Découvrir nos projets">
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/actu/actu.jpeg" width="100%" height="100%" alt="Actualité 3">
-                <div class="carousel-caption d-none d-md-block">
-                    <h1>Info Telecom Strasbourg</h1>
-                    <p>Notre toute nouvelle association débarque à TPS</p>
-                    <input class="favorite styled" type="button" value="Découvrir nos projets">
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="images/actu/actu.jpeg" width="100%" height="100%" alt="Actualité 4">
-                <div class="carousel-caption d-none d-md-block">
-                    <h1>Info Telecom Strasbourg</h1>
-                    <p>Notre toute nouvelle association débarque à TPS</p>
-                    <input class="favorite styled" type="button" value="Découvrir nos projets">
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-
     <!-- Main content -->
     <div class="page">
         <!-- Breadcrumbs -->
-        <div class="breadcrumb-container" aria-label="breadcrumb">
+        <!-- <div class="breadcrumb-container" aria-label="breadcrumb">
             <ol class="breadcrumb">
                 @yield('breadcrumb')
             </ol>
-        </div>
+        </div> -->
 
         <div class="main-content">
             @yield('content')
@@ -264,8 +47,8 @@
     <!-- Footer -->
     @include('footer')
 
-    <!-- Colored navbar -->
     <script src="js/navbar.js"></script>
+    <script src="js/actu.js"></script>
 
 </body>
 
--- website/resources/views/navbar.blade.php
+++ website/resources/views/navbar.blade.php
@@ -1,49 +1,41 @@
-<div class="navbar-bar" id="navbar-bar">
-    <nav class="navbar navbar-expand-sm">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav navbar-nav navbar-center">
-                <li class="nav-item active" , id="active">
-                    <a class="nav-link" href="#">ACCUEIL <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        PÔLES
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="#">Cours & Accompagnements</a>
-                        <a class="dropdown-item" href="#">Applications & Sites Web</a>
-                        <a class="dropdown-item" href="#">Programmation utilitaire</a>
-                        <a class="dropdown-item" href="#">Compétitions</a>
-                        <a class="dropdown-item" href="#">Jeux Vidéos</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">PROJETS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">MEMBRES</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">BESOIN D'AIDE</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">CONTACT</a>
-                </li>
-            </ul>
-        </div>
-        <div class="navbar-co">
-            <a class="nav-link" href="#">CONNEXION</a>
-        </div>
-    </nav>
-</div>
-<div class="navbar-brand" id="navbar-brand">
-    <a class="navbar-logo" href="#">
-        <img src="images/logo/logo.png" width="100" height="100" alt="Logo du site">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <a class="navbar-brand" href="#">
+        <img src="images/logo/logo.png" width="90" height="90" alt="Logo du site">
     </a>
-</div>
-</div>
-
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">ACCUEIL <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    PÔLES
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Cours & Accompagnements</a>
+                    <a class="dropdown-item" href="#">Applications & Sites Web</a>
+                    <a class="dropdown-item" href="#">Programmation utilitaire</a>
+                    <a class="dropdown-item" href="#">Compétitions</a>
+                    <a class="dropdown-item" href="#">Jeux Vidéos</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">PROJETS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">MEMBRES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">BESOIN D'AIDE</a>
+            </li>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+            </li>
+            <a href="#" class="btn btn-rounded btn-primary connexion" type="button">CONNEXION</a>
+        </ul>
+    </div>
+    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</nav>
