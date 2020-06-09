b8853aa6de4a5bc57b60c4b79cff4d2b49b8ae27
laullier <hugo.laullier@gmail.com>
2020-06-09 18:34:29 -0400

    Comments on javascript pages

--- website/public/js/navbar.js
+++ website/public/js/navbar.js
@@ -1,3 +1,6 @@
+// Permet de faire évoluer la couleur de la barre de navigation (de transparent 
+// à bleu) en scrollant
+
 $(document).on('scroll', function(e) {
     var rgba = $(document).scrollTop() / 500;
     $('.fixed-top').css('background-color', 'rgba(92, 111, 163,' + rgba + ')');
