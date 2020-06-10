c59ba1881b10091c86eac28752f10468fc308871
laullier <hugo.laullier@gmail.com>
2020-06-10 18:40:15 -0400

    home page structure

--- website/public/js/actu.js
+++ website/public/js/actu.js
@@ -1,7 +1,7 @@
 // permet de faire en sorte que le carousel fasse exactement la taille de l'écran
 // de l'utilisateur
 
-var $item = $('.carousel-item');
+var $item = $('#carouselExampleIndicators .carousel-item');
 var $wHeight = $(window).height();
 
 $item.height($wHeight);
@@ -10,4 +10,26 @@ $item.addClass('full-screen');
 $(window).on('resize', function() {
     $wHeight = $(window).height();
     $item.height($wHeight);
+});
+
+$('#recipeCarousel').carousel({
+    interval: 10000
+})
+
+$('#nos-projets .carousel .carousel-item').each(function() {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
 });
