468c3167262330a88c4c0b8789dffc89fcd06313
Thomas Rives <thomas.rives29@gmail.com>
2020-06-11 15:00:26 +0200

    Ajout de commentaire dans le model Pole

--- website/app/Pole.php
+++ website/app/Pole.php
@@ -8,11 +8,13 @@ class Pole extends Model
 {
     protected $guarded = [];
 
+	/* Return the responsible of the pole */
     public function respo()
     {
         return $this->belongsTo(User::class);
     }
 
+	/* Return all the projects of the pole */
     public function projets()
     {
         return $this->hasMany(Projet::class);
