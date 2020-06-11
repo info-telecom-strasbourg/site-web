a37f696c99a91baa746629539e5d67f393b028aa
laullier <hugo.laullier@gmail.com>
2020-06-10 20:36:22 -0400

    navbar problems fixed

--- website/public/js/website.js
+++ website/public/js/website.js
@@ -13,45 +13,44 @@ $(document).on('scroll', function(e) {
 
     // if the user scrolls to the top, 
     // a black filter is added to the collapse navbar
-    if ($(window).width() < 1200) {
-	    if ($(document).scrollTop() == 0) {
-			$('.navbar-collapse').css('background-color', 'rgba(0, 0, 0, 0.7)');
-		}
-	}
+    if ($(window).width() < 1188) {
+        if ($(document).scrollTop() == 0) {
+            $('.navbar-collapse').css('background-color', 'rgba(0, 0, 0, 0.7)');
+        }
+    }
 })
 
 // Remove hide button that is used to center links 
 // in the navbar when the navbar is collapsed
-if ($(window).width() < 1200) {
-	$(".hidden").remove();
+if ($(window).width() < 1188) {
+    $(".hidden").remove();
 }
 
 /* Resize */
 
-var $item = $('.carousel-item');		// get carousel item
-var $wHeight = $(window).height();		// get window height
+var $item = $('.carousel-item'); // get carousel item
+var $wHeight = $(window).height(); // get window height
 
-$item.height($wHeight);			// set height of carousel item to window height
+$item.height($wHeight); // set height of carousel item to window height
 
 // on resize of the window
 $(window).on('resize', function() {
-    $wHeight = $(window).height();		// get window height
-    $item.height($wHeight);		// set height of carousel item to window height
+    $wHeight = $(window).height(); // get window height
+    $item.height($wHeight); // set height of carousel item to window height
 
     // Remove hide button that is used to center links 
-	// in the navbar when the navbar is collapsed
-	if ($(window).width() < 1200) {
-		$(".hidden").remove();
-		$('.navbar-collapse').css('background-color', 'rgba(0, 0, 0, 0.7)');
-	}
-	else {
-		// Remove hide button that is used to center links 
-		// in the navbar when the navbar is collapsed to be sure that we don't
-		// add it two times, then add it
+    // in the navbar when the navbar is collapsed
+    if ($(window).width() < 1188) {
+        $(".hidden").remove();
+        $('.navbar-collapse').css('background-color', 'rgba(0, 0, 0, 0.7)');
+    } else {
+        // Remove hide button that is used to center links 
+        // in the navbar when the navbar is collapsed to be sure that we don't
+        // add it two times, then add it
 
-		$(".hidden").remove();	// remove it 
-		$(".not-shown").html("<li class='nav-item hidden'><a href='#'' class='btn btn-rounded btn-primary connexion' type='button'>CONNEXION</a></li>");	// add it
+        $(".hidden").remove(); // remove it 
+        $(".not-shown").html("<li class='nav-item hidden'><a href='#'' class='btn btn-rounded btn-primary connexion' type='button'>CONNEXION</a></li>"); // add it
 
-		$('.navbar-collapse').css('background-color', 'transparent');
-	}
+        $('.navbar-collapse').css('background-color', 'transparent');
+    }
 });
