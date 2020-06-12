ad8ef4df75d6c54a0f97b5c67764408de19f99cd
laullier <hugo.laullier@gmail.com>
2020-06-11 21:45:13 -0400

    inversion between map and form

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -362,7 +362,7 @@
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2642.3720999403968!2d7.7347999514380845!3d48.52610033189745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4796ca33e265b8bf%3A0x3a9eb5576462d5e3!2sT%C3%A9l%C3%A9com%20Physique%20Strasbourg!5e0!3m2!1sfr!2sfr!4v1591805091984!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
-            <div class="contact-form col-md-6">
+            <div id="contact_cont" class="contact-form col-md-6">
                 <h2>Formulaire de contact</h2>
                 <form class="contact-form d-flex flex-column align-items-center" action="https://formspree.io/youremail@mail.com" method="POST">
                     <div class="form-group" style="width: 100%;">
