b882c91501f0f4e502fbfbeb9e1def33ffb357d4
laullier <hugo.laullier@gmail.com>
2020-06-14 13:29:22 -0400

    fill form

--- website/resources/views/besoin-aide.blade.php
+++ website/resources/views/besoin-aide.blade.php
@@ -14,33 +14,32 @@
     <div class="formulaire-besoin-aide">
         <form class="contact-form d-flex flex-column align-items-center" action="https://formspree.io/youremail@mail.com" method="POST">
             <div class="form-group" style="width: 100%;">
-                <label for="exampleFormControlSelect1">Type de problème</label>
+                <label for="exampleFormControlSelect1">Type de demande</label>
                 <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Connexion Wi-Fi Eduroam automatique</option>
+                    <option>Synconisation boîte mail Unistra sur boîte mail perso</option>
+                    <option>Besoin d'une machine virtuelle</option>
+                    <option>Problème avec un logiciel</option>
+                    <option>Fichiers supprimés par erreur</option>
+                    <option>Problème avec le terminal</option>
+                    <option>Utilisation git</option>
+                    <option>Autre</option>
                 </select>
             </div>
             <div class="form-group" style="width: 100%;">
                 <label for="exampleFormControlSelect1">Appareil</label>
                 <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Ordinateur</option>
+                    <option>Téléphone</option>
                 </select>
             </div>
             <div class="form-group" style="width: 100%;">
                 <label for="exampleFormControlSelect1">Système d'exploitation</label>
                 <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                    <option>Linux</option>
+                    <option>Windows</option>
+                    <option>Android</option>
+                    <option>iOS</option>
                 </select>
             </div>
             <div class="form-group" style="width: 100%;">
@@ -48,7 +47,7 @@
                 <input type="file" class="form-control-file" id="exampleFormControlFile1">
             </div>
             <div class="form-group" style="width: 100%;">
-            <label for="exampleFormControlFile1">Description du problème</label>
+            <label for="exampleFormControlFile1">Description de la demande</label>
                 <textarea class="form-control" type="text" placeholder="Message" rows="9" name="name" style="resize: none;" required></textarea>
             </div>
             <button type="submit" class="btn btn-rounded btn-primary" style="width: 200px;">Envoyer</button>
