8f5b4ff14b708a5dfd862eebc79c0288d837a66e
Thomas Rives <thomas.rives29@gmail.com>
2020-06-14 18:33:31 +0200

    création d'une vue pour créer les cours

---
+++ website/resources/views/poles/cours/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.layout')
+
+@section('title')
+Création d'un cours
+@endsection
+
+@section('content')
+
+<div class="container">
+	<form class="" action="{{ route('cours.store') }}" method="post" enctype="multipart/form-data">
+		@csrf
+
+		<div class="form-group">
+			<label for="title">Titre</label>
+
+			<input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus>
+
+			@error('title')
+			<span class="invalid-feedback" role="alert">
+				<strong>{{ $message }}</strong>
+			</span>
+			@enderror
+		</div>
+
+		<div class="form-group">
+			<label for="desc">Description</label>
+
+			<div class="control">
+				<textarea class="form-control @error('desc') is-invalid @enderror" id="desc" name="desc" rows="3" required>{{ old('desc') }}</textarea>
+			</div>
+
+			@error('desc')
+			<span class="invalid-feedback" role="alert">
+				<strong>{{ $message }}</strong>
+			</span>
+			@enderror
+		</div>
+
+
+		<div class="form-group">
+			<label for="link_support">Choose a file</label>
+			<br>
+			<input type="file" id="link_support" name="link_support[]" accept="public/supports/pdf" multiple>
+		</div>
+
+		<button type="submit" class="btn btn-primary btn-rounded">AJOUTER</button>
+	</form>
+</div>
+@endsection
