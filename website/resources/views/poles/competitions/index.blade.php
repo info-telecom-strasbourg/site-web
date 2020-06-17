72e9e59436b5928252e30f283c4d8c319c424107
Thomas Rives <thomas.rives29@gmail.com>
2020-06-17 21:11:46 +0200

    Réorganisation pour le pole compétition

---
+++ website/resources/views/poles/competitions/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container">
+
+    <div class="content-fluid">
+        <h1 class="title lg text-center">
+            Pôle {{ $pole->title }}
+        </h1>
+        <hr class="line-under-title">
+        <div>
+            <p>{{ $pole->desc }}</p>
+            <h4 class="title md text-left">Liste des cours</h4>
+			<div class="container">
+				@forelse ($compets as $compet)
+					<div class="row align-items-center">
+						<div class="col-auto sep-chevr">
+							<i id="chevron-date-supports" class="fas fa-chevron-right fa-2x">
+							</i>
+						</div>
+						<div class="col sep-chevr">
+							<a href="/poles/cours/{{ $compet->id }}" class="link-black">{{ $compet->title }}</a>
+						</div>
+					</div>
+				@empty
+					<div>
+						Il n'y a pas de compétitions disponibles.
+					</div>
+				@endforelse
+			</div>
+	    </div>
+	</div>
+</div>
+@endsection
