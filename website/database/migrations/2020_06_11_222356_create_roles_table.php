0586251969b32b4397506eacd33b34998b7a2c94
Clara Schild <clara.schild@gmail.com>
2020-06-17 15:47:11 +0200

    add field to roles database table

--- website/database/migrations/2020_06_11_222356_create_roles_table.php
+++ website/database/migrations/2020_06_11_222356_create_roles_table.php
@@ -16,6 +16,7 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('role');
+            $table->string('poste');
             $table->timestamps();
         });
     }
