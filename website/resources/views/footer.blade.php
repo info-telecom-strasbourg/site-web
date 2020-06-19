cda4c3e27d009af82b33c7f8184de72964f4331e
Clara Schild <clara.schild@gmail.com>
2020-06-19 18:23:05 +0200

    fix responsive footer

--- website/resources/views/footer.blade.php
+++ website/resources/views/footer.blade.php
@@ -1,30 +1,22 @@
 <footer class="page-footer font-small teal pt-4" id="footer">
-    <div class="container-fluid text-center text-md-left">
-        <div class="row">
-            <div class="col-md-6 mt-md-0 mt-3">
+    <div class="container-fluid text-center text-lg-left">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 mt-lg-0 mt-3">
                 <h5 class="text-uppercase font-weight-bold">Info Télécom Strasbourg</h5>
                 <p>300 Bd Sébastien Brant <br> CS 10414 - F 67400 ILLKIRCH-GRAFFENSTADEN</p>
             </div>
-            <hr class="clearfix w-100 d-md-none pb-3">
-            <div class="col-md-6 mb-md-0 mb-3">
+            <hr class="clearfix w-100 d-lg-none pb-3">
+            <div class="col-lg-6 mb-lg-0 mb-3">
                 <div class="social-buttons">
-                    <ul class="list-unstyled list-inline text-center">
-                        <li class="list-inline-item">
-                            <a type="button" class="github" href="#">
-                                <i class="fab fa-github fa-4x"></i>
+                            <a type="button" class="github" href="#" style="margin-left: 50px">
+                                <i class="fab fa-github fa-4x fa-lg mr-3"></i>
                             </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a type="button" class="github" href="#">
-                                <i class="fab fa-facebook fa-4x"></i>
+                            <a type="button" class="github" href="#" style="margin-left: 50px">
+                                <i class="fab fa-facebook fa-4x fa-lg mr-3"></i>
                             </a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a type="button" class="github" href="#">
-                                <i class="fas fa-envelope fa-4x"></i>
+                            <a type="button" class="github" href="#" style="margin-left: 50px">
+                                <i class="fas fa-envelope fa-4x fa-lg mr-3"></i>
                             </a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
