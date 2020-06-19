8001a38ac352da091f852f96451cb50ba59aeb8a
Zetsyog <zetsyog@gmail.com>
2020-06-19 15:15:08 +0200

    Projet controller done Woring projet.show view

---
+++ website/app/Http/Requests/ProjetRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProjetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     * TODO
+     * 
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|max:255',
+            'link_github' => 'url',
+            'link_download' => 'url',
+            'link_doc' => 'url',
+            'chef_projet_id' => 'integer',
+            'pole_id' => 'integer'
+        ];
+    }
+}
