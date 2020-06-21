a0638444e48b3d3235e8f03e5146f41376d2c733
Zetsyog <zetsyog@gmail.com>
2020-06-21 16:59:16 +0200

    Projet routes uses resources

--- website/routes/web.php
+++ website/routes/web.php
@@ -21,10 +21,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/projets', 'ProjetController@index')->name('projet.index');
-Route::get('/projet/create', 'ProjetController@create')->name('projet.create');
-Route::post('/projet/create', 'ProjetController@store')->name('projet.store');
-Route::get('/projet/edit/{projet}', 'ProjetController@edit')->name('projet.edit');
-Route::patch('/projet/edit', 'ProjetController@update')->name('projet.update');
-Route::delete('/projet/delete/{id}', 'ProjetController@destroy')->name('projet.destroy');
-Route::get('/projet/{projet}', 'ProjetController@show')->name('projet.show');
+Route::resources([
+    'projets' => "ProjetController"
+]);
