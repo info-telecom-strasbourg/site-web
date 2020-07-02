e2e959fce7d959892e29ff57844e7ff09dfe9f2a
Clara Schild <clara.schild@gmail.com>
2020-07-02 03:22:13 +0200

    modify form besoin aide

--- website/resources/views/besoin-aide.blade.php
+++ website/resources/views/besoin-aide.blade.php
@@ -9,10 +9,11 @@
 
 @section('content')
 
-<section id="besoin-aide">
+<div class="container">
     <h1 class="title lg text-center"> Besoin d'aide </h1>
     <hr class="line-under-title">
-    <div class="formulaire-besoin-aide">
+
+    <div class="container pt-3">
         @guest
             <div class="alert alert-warning alert-dismissible fade show" role="alert" style="margin-top: 50px">
                 Veuillez-vous <strong><a href="/login">connecter</a></strong> pour accéder à ce service.
@@ -21,10 +22,12 @@
                 </button>
             </div>
         @else
-            <form class="contact-form d-flex flex-column align-items-center" action="https://formspree.io/youremail@mail.com" method="POST">
-                <div class="form-group" style="width: 100%;">
-                    <label for="exampleFormControlSelect1">Type de demande</label>
-                    <select class="form-control" id="exampleFormControlSelect1">
+            <form action="https://formspree.io/youremail@mail.com" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="type" class="form-title-small">
+                        Type de demande
+                    </label>
+                    <select class="form-control" id="type" name="type">
                         <option>Connexion Wi-Fi Eduroam automatique</option>
                         <option>Synconisation boîte mail Unistra sur boîte mail perso</option>
                         <option>Besoin d'une machine virtuelle</option>
@@ -35,35 +38,42 @@
                         <option>Autre</option>
                     </select>
                 </div>
-                <div class="form-group" style="width: 100%;">
-                    <label for="exampleFormControlSelect1">Appareil</label>
-                    <select class="form-control" id="exampleFormControlSelect1">
+                <div class="form-group">
+                    <label for="appareil" class="form-title-small">
+                        Appareil
+                    </label>
+                    <select class="form-control" id="appareil" name="appareil" class="form-title-small">
                         <option>Ordinateur</option>
                         <option>Téléphone</option>
                     </select>
                 </div>
-                <div class="form-group" style="width: 100%;">
-                    <label for="exampleFormControlSelect1">Système d'exploitation</label>
-                    <select class="form-control" id="exampleFormControlSelect1">
+                <div class="form-group">
+                    <label for="os" class="form-title-small">
+                        Système d'exploitation
+                    </label>
+                    <select class="form-control" id="os" name="os">
                         <option>Linux</option>
                         <option>Windows</option>
                         <option>Android</option>
                         <option>iOS</option>
                     </select>
                 </div>
-                <div class="form-group" style="width: 100%;">
-                    <label for="exampleFormControlFile1">Images</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                <div class="form-group">
+                    <label for="images" class="form-title-small">Images</label>
+                    <input type="file" class="form-control-file" id="images" name="images[]" multiple>
                 </div>
-                <div class="form-group" style="width: 100%;">
-                    <label for="exampleFormControlFile1">Description de la demande</label>
-                    <textarea class="form-control" type="text" placeholder="Message" rows="9" name="name" style="resize: none;" required></textarea>
+                <div class="form-group">
+                    <label for="desc" class="form-title-small">Description de la demande</label>
+                    <textarea class="form-control" type="text" placeholder="Message" rows="9" id="desc" name="desc" style="resize: none;" required></textarea>
                 </div>
-                <button type="submit" class="btn btn-rounded btn-primary" style="width: 200px;">Envoyer</button>
+                
+                <div class="text-center" style="margin-top:25px; margin-bottom:25px">
+                    <button type="submit" class="btn btn-primary btn-rounded">Envoyer</button>
+                </div>
             </form>
         @endguest
     </div>
-</section>
+</div>
 
 
 @endsection
