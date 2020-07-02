427f90009055647e4deb5aa0fdd1e23ff101f3e7
Clara Schild <clara.schild@gmail.com>
2020-07-02 13:51:52 +0200

    remove row div in edit pole

--- website/resources/views/poles/edit.blade.php
+++ website/resources/views/poles/edit.blade.php
@@ -16,44 +16,42 @@
     <hr class="line-under-title">
     
     <div class="container pt-3">
-    	<div class="row">
-			<form action="/poles/{{ $pole->id }}" method="POST">
-				@csrf
-				@method('PUT')
+    	<form action="/poles/{{ $pole->id }}" method="POST">
+    		@csrf
+    		@method('PUT')
 
-				<!-- Titre -->
-				<div class="form-group">
-					<label class="form-title-small" for="title">Titre</label>
-					<div class="control">
-						<input class="form-control @error('title') is-invalid @enderror" type="text" value="{{ $pole->title }}" id="title" name="title" required>
-					</div>
-				</div>
-				@error('title')
-					<span class="invalid-feedback" role="alert">
-						<strong>{{ $message }}</strong>
-					</span>
-				@enderror
+    		<!-- Titre -->
+    		<div class="form-group">
+    			<label class="form-title-small" for="title">Titre</label>
+    			<div class="control">
+    				<input class="form-control @error('title') is-invalid @enderror" type="text" value="{{ $pole->title }}" id="title" name="title" required>
+    			</div>
+    		</div>
+    		@error('title')
+    		<span class="invalid-feedback" role="alert">
+    			<strong>{{ $message }}</strong>
+    		</span>
+    		@enderror
 
-				<!-- Decription -->
-				<div class="form-group">
-					<label class="form-title-small" for="desc">Decription</label>
-					<div class="control">
-						<textarea class="form-control desc @error('desc') is-invalid @enderror" id="desc" name="desc" rows="3" required>{{ $pole->desc }}</textarea>
-					</div>
-				</div>
-				@error('desc')
-					<span class="invalid-feedback" role="alert">
-						<strong>{{ $message }}</strong>
-					</span>
-				@enderror
+    		<!-- Decription -->
+    		<div class="form-group">
+    			<label class="form-title-small" for="desc">Decription</label>
+    			<div class="control">
+    				<textarea class="form-control desc @error('desc') is-invalid @enderror" id="desc" name="desc" rows="3" required>{{ $pole->desc }}</textarea>
+    			</div>
+    		</div>
+    		@error('desc')
+    		<span class="invalid-feedback" role="alert">
+    			<strong>{{ $message }}</strong>
+    		</span>
+    		@enderror
 
-				<!-- Bouton submit -->
-				<div class="text-center" style="margin-top:25px; margin-bottom:25px">
-					<button type="submit" class="btn btn-primary btn-rounded">Enregistrer</button>
-				</div>
+    		<!-- Bouton submit -->
+    		<div class="text-center" style="margin-top:25px; margin-bottom:25px">
+    			<button type="submit" class="btn btn-primary btn-rounded">Enregistrer</button>
+    		</div>
 
-			</form>
-		</div>
+    	</form>
 	</div/>
 </div>
 @endsection
