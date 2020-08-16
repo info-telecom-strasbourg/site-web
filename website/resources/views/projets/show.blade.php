03767cce168473237912b312e211becd7cf5bdd0
laullier <hugo.laullier@gmail.com>
2020-08-16 23:17:11 +0200

    Formatting

--- website/resources/views/projets/show.blade.php
+++ website/resources/views/projets/show.blade.php
@@ -95,25 +95,16 @@
         <h4 class="title md text-center">Timeline du projet</h4>
         <div class="container mt-5 mb-5">
             <div class="row">
-                <div class="col-md-6 offset-md-3">
-                    <ul class="timeline">
-                        <li>
-                            <a target="_blank" href="https://www.totoprayogo.com/#">New Web Design</a>
-                            <a href="#" class="float-right">21 March, 2014</a>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
-                        </li>
-                        <li>
-                            <a href="#">21 000 Job Seekers</a>
-                            <a href="#" class="float-right">4 March, 2014</a>
-                            <p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
-                        </li>
-                        <li>
-                            <a href="#">Awesome Employers</a>
-                            <a href="#" class="float-right">1 April, 2014</a>
-                            <p>Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...</p>
-                        </li>
-                    </ul>
-                </div>
+                <ul class="timeline">
+                    <li>
+                        <a href="#">15 Août 2020</a>
+                        <p>Thomas commence un dab.</p>
+                    </li>
+                    <li>
+                        <a href="#">16 Août 2020</a>
+                        <p>Thomas finit un dab.</p>
+                    </li>
+                </ul>
             </div>
         </div>
         <!-- Images of the project -->
