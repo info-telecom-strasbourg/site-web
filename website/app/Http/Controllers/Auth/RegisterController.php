8fda17983916e5bd724ca8483697e9c4fa7d80cb
laullier <hugo.laullier@gmail.com>
2020-08-19 14:04:10 +0200

    Petit bug de dernière seconde

--- website/app/Http/Controllers/Auth/RegisterController.php
+++ website/app/Http/Controllers/Auth/RegisterController.php
@@ -114,10 +114,6 @@ class RegisterController extends Controller
 			'year' => $data['year'],
         ]);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5b1494e9b461fdb0d0bfe121e4b9e4bb5e71658a
         $user->save();
 
         return $user;
