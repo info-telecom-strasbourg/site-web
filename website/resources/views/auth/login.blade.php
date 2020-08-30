078fe5a9b47e716741cec2691f2eab1f63f5afdd
laullier <hugo.laullier@gmail.com>
2020-08-30 14:32:43 +0200

    redirection login page

--- website/resources/views/auth/login.blade.php
+++ website/resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
 <!-- Button to get to the previous page -->
-<a class="btn btn-primary btn-rounded" style="font-size: 20px; position: absolute; top: 10px; left: 10px; padding: 5px 15px; text-decoration: none; z-index: 10000; color: white;" data-toggle="tooltip" data-placement="right" title="Retour vers la page de connexion." onclick="window.history.back()">
+<a class="btn btn-primary btn-rounded" style="font-size: 20px; position: absolute; top: 10px; left: 10px; padding: 5px 15px; text-decoration: none; z-index: 10000; color: white;" data-toggle="tooltip" data-placement="right" title="Retour vers la page de connexion." href="/">
     <i class="fas fa-angle-left" ></i>
 </a>
 
