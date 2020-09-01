3911166b60325082d0fb90e5a243ef78fabb2847
Thomas Rives <thomas.rives29@gmail.com>
2020-09-01 21:16:19 +0200

    fix mail change password

--- website/resources/views/emails/change-password.blade.php
+++ website/resources/views/emails/change-password.blade.php
@@ -5,7 +5,7 @@
 # Bienvenue à ITS !
 
 
-Nous t'avons assigner le mot de passe suivant: {{ }}.
+Nous t'avons assigner le mot de passe suivant: {{ $password }}.
 Nous te recommandons vivement de le changer pour ne pas l'oublier ;).
 
 Toute l'équipe d'ITS te souhaite la bienvenue au sein de l'association, et a hâte de te revoir !
