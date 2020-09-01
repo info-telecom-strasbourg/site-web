58cff0adce0f48934b94261109f15861468b52b2
cschild <clara.schild@gmail.co>
2020-09-01 15:30:22 +0200

    translate reset password email

---
+++ website/app/Notifications/ResetPassword.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+
+
+class ResetPassword extends ResetPasswordNotification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $link = url("/password/reset/" . $this->token);
+
+        
+        return (new MailMessage)
+            ->subject('Reset Password Notification')
+            ->line('Vous recevez cet email car nous avons reçu une demande de réinitialisation de mot de passe pour votre compte.')
+            ->action('Réinitialiser le mot de passe', $link)
+            ->line('Ce lien de réinitialisation expirera dans :count minutes.
+            ', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')])
+            ->line('Si vous n\'avez pas demandé la réinitialisation de votre mot de passe, aucune autre action n\'est requise.
+            ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
