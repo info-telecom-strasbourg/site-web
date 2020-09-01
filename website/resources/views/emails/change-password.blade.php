b15413b7a35af5dc2f1e0de22f3ed2be8a7b0e63
Thomas Rives <thomas.rives29@gmail.com>
2020-09-01 21:38:24 +0200

    button in email

--- website/resources/views/emails/change-password.blade.php
+++ website/resources/views/emails/change-password.blade.php
@@ -8,6 +8,10 @@
 Nous t'avons assigner le mot de passe suivant: {{ $password }}.
 Nous te recommandons vivement de le changer pour ne pas l'oublier ;).
 
+@component('mail::button', 'url' => '/users/' . $id)
+Changer mon mot de passe
+@endcomponent
+
 Toute l'équipe d'ITS te souhaite la bienvenue au sein de l'association, et a hâte de te revoir !
 
 Cordialement,
