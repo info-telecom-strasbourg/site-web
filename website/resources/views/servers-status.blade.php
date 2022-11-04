2bfdb5151913ff25c799a8df4135d4f64442c795
FelixLusseau <felix.lusseau@gmail.com>
2022-11-04 12:30:41 +0100

    Pages serveurs OK + menu

--- website/resources/views/servers-status.blade.php
+++ website/resources/views/servers-status.blade.php
@@ -1,15 +1,23 @@
 <!-- Servers stats page -->
 @extends('layouts.layout')
 
-@section('title', 'Statuts des serveurs ITS')
+@section('title', 'Statut des serveurs ITS en temps réel')
 
 @section('breadcrumb')
 <li class="breadcrumb-item"><a href="/"><i class="fas fa-home"></i></a></li>
-<li class="breadcrumb-item active">Statuts des serveurs ITS</li>
+<li class="breadcrumb-item active">Statut des serveurs ITS</li>
 @endsection
 
 @section('content')
 
-<iframe src="https://uptime.its-tps.fr/status/its" width="100%"></iframe>
+<div class="container">
+    <h1 class="title lg text-center">
+        Statut des serveurs ITS en temps réel
+    </h1>
+    <hr class="line-under-title">
+    <br><br>
+    <p>Lien vers la page de statut : <a href="https://uptime.its-tps.fr/status/its" target="_blank">https://uptime.its-tps.fr/status/its</a></p>
+    <iframe src="https://uptime.its-tps.fr/status/its" width="100%" height="2000px"></iframe>
+</div>
 
 @endsection
